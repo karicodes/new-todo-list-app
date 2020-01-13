4a7a11c8a0c9cfe4ed4e59a6662735fca9a44712
Karen <karenbrady92@hotmail.co.uk>
2020-01-13 00:29:08 +0000

    added state

--- src/App.js
+++ src/App.js
@@ -7,13 +7,52 @@ import './App.css';
 import uuid from 'uuid/v4';
 
 class App extends Component {
+
+  state = {
+    tasks: [
+      {
+        id: uuid(),
+        taskName: "Learn HTML",
+        complete: true,
+        dueDate: "2019-11-28"
+      },
+      {
+        id: uuid(),
+        taskName: "Learn CSS",
+        complete: true,
+        dueDate: "2019-12-02"
+      },
+      {
+        id: uuid(),
+        taskName: "Learn JavaScript",
+        complete: true,
+        dueDate: "2019-11-11"
+      },
+      {
+        id: uuid(),
+        taskName: "Learn React",
+        complete: false,
+        dueDate: "2020-01-06"
+      }
+    ]
+  }
+
   render() {
     return (
       <div className="container">
         <Header />
         <AddTask />
         <OutstandingCount />
-        <Todos />
+        {this.state.tasks.map(task => {
+          return <Todos
+            key={task.id}
+            taskName={task.taskName}
+            dueDate={task.dueDate}
+            complete={task.complete}
+            id={task.id}
+          />
+        })}
+      
 
       </div>
     );
